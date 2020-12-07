a7ecc47fee930729f8373350eb5b1a28ed429ac8
Roman Tymofieiev <rdtymofieiev@gmail.com>
2020-12-08 00:26:54 +0200

    Added HW for Lesson 6

---
+++ HW_6/HW_T1.py
@@ -0,0 +1,7 @@
+numbers = list(range(0,101))
+
+even = [num_even for num_even in numbers if num_even % 2 == 0]
+odd = [num_odd for num_odd in numbers if num_odd % 3 == 0]
+div_except_23 = [num_other for num_other in numbers if not num_other % 2 == 0 and not num_other % 3 == 0]
+
+print(div_except_23)
