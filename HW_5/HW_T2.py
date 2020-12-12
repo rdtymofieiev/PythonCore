009da229e123f060ad51e598db6ce1e30f7b67e9
Roman Tymofieiev <rdtymofieiev@gmail.com>
2020-12-12 04:05:09 +0200

    updated

---
+++ HW_5/HW_T2.py
@@ -0,0 +1,7 @@
+correct_login = 'First'
+user_login = (input('Please input the login '))
+while not user_login == 'First':
+    print('Your login is not correct. Try again')
+    user_login = (input('Please input the login '))
+
+print('CORRECT')
